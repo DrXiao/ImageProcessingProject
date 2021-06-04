8fd5e4f23d1516463e576c54dff69470fd982703
Dr.Xiao <zxc71230@gmail.com>
2021-06-04 23:55:50 +0800

    readVedio can play audio

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import cv2
-
+from ffpyplayer.player import MediaPlayer
 video_path = 'movie/seal.mp4'
 
 """
@@ -9,11 +9,11 @@ Input :
         None
 Output:
         播放海豹ㄉ影片
-        暫時沒有聲音
+        有聲音，但是聲音會先播放完：|
 """
 def readViedo():
     video = cv2.VideoCapture(video_path)
-
+    player = MediaPlayer(video_path)
     fps = int(video.get(cv2.CAP_PROP_FPS))
     frame_nums = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     for frame_idx in range(frame_nums):
@@ -23,8 +23,9 @@ def readViedo():
             break
         cv2.imshow('frame', frame)
         cv2.waitKey(1000//fps)
+        audio_frame, val = player.get_frame()
     video.release()
-    cv2.destoryAllWindows()
+    cv2.destroyAllWindows()
 
 
 def main():
