6a131c3c4fd70dabcb11d377f0df334b2d595d8f
Dr.Xiao <zxc71230@gmail.com>
2021-06-08 13:27:00 +0800

    Adding .avi file and modifying main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import cv2
 from ffpyplayer.player import MediaPlayer
-video_path = 'movie/seal.mp4'
+video_path = 'movie/seal.avi'
 
 """
 Utility : 
@@ -16,15 +16,22 @@ def readViedo():
     player = MediaPlayer(video_path)
     fps = int(video.get(cv2.CAP_PROP_FPS))
     frame_nums = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    print('%x'%(fourcc))
+    width, height = int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter('movie/output.avi',  fourcc, fps, (width, height))
     for frame_idx in range(frame_nums):
         ret, frame = video.read()
         if not ret:
             print("Can't not receive frame")
             break
+        frame = cv2.flip(frame, 1)
+        out.write(frame)
         cv2.imshow('frame', frame)
         cv2.waitKey(1000//fps)
         audio_frame, val = player.get_frame()
     video.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
