c09fe50be4218dc72bc46f4b2c7c6165705ca835
Teazer0 <jasmine24611755@gmail.com>
2021-06-14 23:49:45 +0800

    T0

--- src/effect_teazer.py
+++ src/effect_teazer.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
+import imutils
 import time
 from ffpyplayer.player import MediaPlayer
 from PIL import Image
-from matplotlib import pyplot
 video_path = 'movie/seal.mp4'
 
 ##
@@ -13,35 +13,31 @@ video_path = 'movie/seal.mp4'
 
 def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 灰階
-    ret, img = cv2.threshold(img, 170, 255, cv2.THRESH_BINARY)  # 門檻
+    ret, img = cv2.threshold(img, 165, 255, cv2.THRESH_BINARY)  # 門檻
+    # fgmask_diff = cv2.cvtColor(fgmask_diff, cv2.COLOR_GRAY2BGR)
     return img
 
 
 lastFrame = None
-fgbg_mog = cv2.bgsegm.createBackgroundSubtractorMOG()
-#fgbg_mog = cv2.createBackgroundSubtractorMOG(history=60, detectShadows=True)
-fgbg_mog2 = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
-fgbg_knn = cv2.createBackgroundSubtractorKNN(detectShadows=True)
-fgbg_gmg = cv2.bgsegm.createBackgroundSubtractorGMG()
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
 
-def img_process(img):
+
+def img_process(orgimg, img):
 
     global lastFrame
     frame_preprocess = preprocess(img)
     if(lastFrame is None):
         lastFrame = frame_preprocess
-    fgmask_diff = np.uint8(cv2.absdiff(frame_preprocess, lastFrame))
+    fgmask_diff = cv2.absdiff(frame_preprocess, lastFrame)
     lastFrame = frame_preprocess
     fgmask_diff = cv2.cvtColor(fgmask_diff, cv2.COLOR_GRAY2BGR)
-
-    # fgmask_mog = fgbg_mog.apply(frame_preprocess)
-    # fgmask_mog2 = fgbg_mog2.apply(frame_preprocess)
-    # fgmask_knn = fgbg_knn.apply(frame_preprocess)
-    # fgmask_gmg = fgbg_gmg.apply(frame_preprocess)
-
-    return fgmask_diff
+    # dst=cv2.addWeighted(orgimg,0.7,fgmask_diff,0.3,0)
+    dst = cv2.bitwise_not(orgimg,fgmask_diff)
+    # dst = cv2.addWeighted(orgimg,0.7,fgmask_diff,0.1,0)
+    # dst = cv2.bitwise_not(fgmask_diff,orgimg)
+    # dst = cv2.addWeighted(orgimg,0.9,dst,0.1,0)  
+    # dst = cv2.bitwise_not(dst,fgmask_diff)
+    return dst
 
 
 """
@@ -55,7 +51,7 @@ Output:
 """
 
 
-def readVideo_T0(filename='movie/output.mp4'):
+def readViedo():
 
     video = cv2.VideoCapture(video_path)
     # player = MediaPlayer(video_path)
@@ -63,18 +59,28 @@ def readVideo_T0(filename='movie/output.mp4'):
     frame_nums = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     width, height = int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(
         video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter(filename,
+    out = cv2.VideoWriter('movie/output1.mp4',
                           0x7634706d, fps, (width, height))
+    ret, frame = video.read()
     for frame_idx in range(frame_nums):
         ret, frame = video.read()
+        orgimg = frame
         if not ret:
             print("Can't not receive frame")
             break
-        frame = np.uint8(img_process(frame))
-        # audio_frame, val = player.get_frame()
-        # cv2.imshow('frame', frame)
+        frame = cv2.flip(frame, 1)  # 圖片翻轉
+        frame = img_process(frame, frame)
         out.write(frame)
+        # audio_frame, val = player.get_frame()
+        cv2.imshow('frame', frame)
         cv2.waitKey(1000//fps)
     video.release()
     out.release()
     cv2.destroyAllWindows()
+
+
+def main():
+    readViedo()
+
+
+main()
